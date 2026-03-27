263e9210fd46f2804647af9ac0f84aece4281ee3
Wissko <95225505+Wissko@users.noreply.github.com>
2026-03-27 17:23:19 +0100

    refactor: simplify home page to Hero + Footer only

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,10 @@
-"use client";
-
 import HeroSection from "@/components/HeroSection";
-import AboutSection from "@/components/AboutSection";
-import SpecialtiesSection from "@/components/SpecialtiesSection";
-import GallerySection from "@/components/GallerySection";
-import SignatureSection from "@/components/SignatureSection";
-import WorkshopsSection from "@/components/WorkshopsSection";
-import VisitSection from "@/components/VisitSection";
 import FooterSection from "@/components/FooterSection";
 
 export default function Home() {
   return (
     <main>
       <HeroSection />
-      <AboutSection />
-      <SpecialtiesSection />
-      <GallerySection />
-      <SignatureSection />
-      <WorkshopsSection />
-      <VisitSection />
       <FooterSection />
     </main>
   );
